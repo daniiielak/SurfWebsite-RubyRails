651b71d46ac6c3403754fd3431b5ef3b52db9fcd
daniiielak <danielaelisabeth.kepper@gmail.com>
2019-05-07 23:38:01 +0100

    devise adjustments and about page finalisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
-    
+
   protect_from_forgery prepend: true, with: :exception  
 
   skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
   skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'destroy' }
+  skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'update' }
   skip_before_action :verify_authenticity_token, if: -> { controller_name == 'registrations' && action_name == 'create' }
   skip_before_action :verify_authenticity_token, if: -> { controller_name == 'ActionController' && action_name == 'create' }
-    skip_before_action :verify_authenticity_token, if: -> { controller_name == 'users' && action_name == 'destroy' }
+  skip_before_action :verify_authenticity_token, if: -> { controller_name == 'ActionController' && action_name == 'update' }
+  skip_before_action :verify_authenticity_token, if: -> { controller_name == 'users' && action_name == 'destroy' }
+
 
 end
